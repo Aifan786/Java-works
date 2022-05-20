9d05f364fabc4208f82dc62f2a6a421ece61008d
Aifanul Islam <sufyanislam00@gmail.com>
2022-05-20 12:08:50 +0530

    Assingments

---
+++ src/com/Day4/EvenOdd.java
@@ -0,0 +1,31 @@
+package com.Day4;
+
+public class EvenOdd {
+
+int num;
+	
+	void numberType() {
+		
+		if(num<0) {
+			System.out.println("Error");
+		}
+		else if(num%2!=0) {
+			System.out.println(num);
+		}
+		else {
+			for(int i=1; i<=10; i++) {
+				if((num+i)%10==0) {
+					System.out.println(num+i);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		EvenOdd n1 = new EvenOdd();
+		n1.num = 62;
+		
+		n1.numberType();
+	}
+}
